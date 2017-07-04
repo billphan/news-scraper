dd55140219e4a96675305fe2f9a9625405a94da4
Bill Phan <billphan25@gmail.com>
2017-07-03 22:30:54 -0700

    Created scraper with class and methods.

---
+++ main.py
@@ -0,0 +1,35 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+#  Created Scraper class
+class Scraper:
+    def __init__(self, site):
+        self.site = site
+
+    def scrape(self):
+        response = urllib.request.urlopen(self.site)
+        html = response.read()
+        parser = "html.parser"
+        soup = BeautifulSoup(html, parser)
+
+        for tag in soup.find_all("a"):
+            url = tag.get("href")
+            if url is None:
+                continue
+            if "html" in url:
+                print("\n" + url)
+
+    # Scrape method that gets headlines and saves in a txt file.
+    def scrape(self):
+        response = urllib.request.urlopen(self.site)
+        html = response.read()
+        soup = BeautifulSoup(html, 'html.parser')
+        with open("output.txt", "w") as f:
+            for tag in soup.find_all('a'):
+                url = tag.get('href')
+                if url and 'html' in url:
+                    print("\n" + url)
+                    f.write(url + "\n")
+
+news = "https://news.google.com/"
+Scraper(news).scrape()
